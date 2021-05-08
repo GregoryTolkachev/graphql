973af8d8896fe4cdf1041955713cf3c2f7a2442a
Greg <russki_canuck@hotmail.com>
2021-05-08 14:21:49 -0400

    Lab6

---
+++ gql/resolvers.js
@@ -0,0 +1,23 @@
+const teacher = require('../model/Teacher');
+const course = require('../model/Course');
+const student = require('../model/Student');
+
+module.exports = {
+    Query: {
+        // teacher: (args) => teacher.getTeacherById(args),
+        teacher: (parent, args) => teacher.getTeacherById(parent, args),
+        teachers: teacher.getAllTeachers,
+        courses: course.getAllCourses,
+        students: student.getAllStudents
+    },
+    Teacher: {
+        courses: (teacher) => course.getCoursesByTeacher(teacher)
+    },
+    Course: {
+        students: (course) => student.getStudentsByCourse(course),
+        teacher: (course) => teacher.getTeacherByCourse(course)
+    },
+    Student: {
+        courses: (student) => course.getCoursesByStudent(student)
+    }
+}
